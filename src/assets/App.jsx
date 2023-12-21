7f157fde7a369fe9ed89538283936584ee1ce23b
joonaro <cupetus@gmail.com>
2023-12-21 19:21:08 +0200

    cleaned code

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -3,11 +3,12 @@ let boolean = false;
 function App() {
   const [search, setSearch] = useState("");
   const [arraySorted, setArraySorted] = useState();
-
   const searchHandler = (e) => {
     setSearch(e.target.value);
   }
-
+  //take first two values and compare them if a is bigger than b then b becomes a and a becomes b.
+  //then move to the next pair b,c and do the same until you reach the end of the array and start again
+  //create a variable with a boolean value. if false then go through the loop again
   const arranger = () => {
     boolean = true;
     for (a = 0, b = 1; b < arrLength; a++, b++) {
@@ -15,44 +16,17 @@ function App() {
         [arrToSort[a], arrToSort[b]] = [arrToSort[b], arrToSort[a]];
         boolean = false;
       }
-      /*  if (b === arrLength - 1) {
-         //sortedArray.push(arrToSort[b])
-         console.log(sortedArray.unshift(arrToSort[b]));
-         //arrToSort.pop();
-       } */
     }
-    sortedArray.unshift(arrToSort[b])
-    //}
-    console.log(arrToSort);
-    console.log(sortedArray);
+    sortedArray.unshift(arrToSort[b]);
   }
-  //a,b, ...arrayToSort
-  //take first two values and compare them if a is bigger than b then b becomes a and a becomes b.
-  //then move to the next pair b,c and do the same until you reach the end of the array and start again
-  //first is array[0] compared to array[1]
-  //then array[1] compared to array[2]
-  //in the end array[array.length-1]
   const arrToSort = [...search];
   let sortedArray = [];
   let arrLength = arrToSort.length;
   const arraySorter = () => {
     let a, b, rest;
-    console.log(arrToSort);
-    /*  [a, b, ...rest] = arrayToSort;
-     console.log(a, b); */
-    /* if (arrToSort[0] > arrToSort[1]) {
-      [arrToSort[0], arrToSort[1]] = [arrToSort[1], arrToSort[0]];
-    } */
-    //create a variable with a boolean value. if false then go through the loop again
-    console.log(arrToSort);
-    //for (length = arrToSort.length; length > 1; length--) {
-
-    console.log(boolean);
     if (boolean = false) {
       arranger()
-      console.log(arrToSort);
     }
-
   }
   return (
     <>
@@ -62,5 +36,4 @@ function App() {
     </>
   )
 }
-
 export default App
